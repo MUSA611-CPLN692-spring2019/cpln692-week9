3042cfa4233bdf08922e6492671abb8e72ecfd4e
angelicakim28 <hskim28@upenn.edu>
2019-04-21 20:16:40 -0400

    data versions

--- clusters/clusters.js
+++ clusters/clusters.js
@@ -3,18 +3,18 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYW5nZWxpY2FraW0yOCIsImEiOiJjajg0dW95ZzYwZXU0M
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
-    zoom: 3
+    center: [-75.1652, 39.9526],
+    zoom: 12
 });
 
 map.on('load', function() {
     // Add a new source from our GeoJSON data and set the
     // 'cluster' option to true. GL-JS will add the point_count property to your source data.
-    map.addSource("earthquakes", {
-        type: "geojson",
+    map.addSource("phillyschools", {
+        type: "json",
         // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
         // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data: "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
+        data: "https://raw.githubusercontent.com/angelicakim28/cpln692-week9/master/clusters/phillySchools.js",
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -23,7 +23,7 @@ map.on('load', function() {
     map.addLayer({
         id: "clusters",
         type: "circle",
-        source: "earthquakes",
+        source: "phillyschools",
         filter: ["has", "point_count"],
         paint: {
             // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -55,7 +55,7 @@ map.on('load', function() {
     map.addLayer({
         id: "cluster-count",
         type: "symbol",
-        source: "earthquakes",
+        source: "phillyschools",
         filter: ["has", "point_count"],
         layout: {
             "text-field": "{point_count_abbreviated}",
@@ -67,7 +67,7 @@ map.on('load', function() {
     map.addLayer({
         id: "unclustered-point",
         type: "circle",
-        source: "earthquakes",
+        source: "phillyschools",
         filter: ["!", ["has", "point_count"]],
         paint: {
             "circle-color": "#11b4da",
@@ -81,7 +81,7 @@ map.on('load', function() {
     map.on('click', 'clusters', function (e) {
         var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
         var clusterId = features[0].properties.cluster_id;
-        map.getSource('earthquakes').getClusterExpansionZoom(clusterId, function (err, zoom) {
+        map.getSource('phillyschools').getClusterExpansionZoom(clusterId, function (err, zoom) {
             if (err)
                 return;
 
